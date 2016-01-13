8857a2d3b4c4ddfb9f8d925d6b849790f76e9138
Santiago <santiago7fergo@gmail.com>
2016-01-12 21:53:27 -0500

    Initial Commit of backEnd and fronEnd projects

---
+++ Back_End/src/main/java/hello/Application.java
@@ -0,0 +1,13 @@
+package hello; /**
+ * Created by santiago_fergo on 1/10/16.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+    @SpringBootApplication
+    public class Application {
+
+        public static void main(String[] args) {
+            SpringApplication.run(Application.class, args);
+        }
+}
